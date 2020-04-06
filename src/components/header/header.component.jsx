cafe6f25a42fea61f1e86adf3f0f45435f8fed06
JilMuriel <djibrilmuriel@gmail.com>
2020-04-06 16:10:34 +0800

    react redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from 'react-redux';
 import { Link } from "react-router-dom";
 import {auth} from '../../firebase/firebase.utils'
 import "./header.styles.scss";
